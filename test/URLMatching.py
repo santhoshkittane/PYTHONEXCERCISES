ee75753c47efa717ca0c6c91ea594e8d55099110
Santhosh Kittane <santhosh.kittane@accionlabs.com>
2026-07-07 21:52:36 +0530

    test

---
+++ test/URLMatching.py
@@ -0,0 +1,28 @@
+import tldextract
+
+
+def urlmatch(str1):
+    flag = True
+    if str1.startswith(("http","www")) and str1.endswith((".com",".co.in","/")):
+            pass
+    else:
+        flag = False
+    return flag
+
+
+def get_website_name(url):
+    # Extract parts from the URL
+    extracted = tldextract.extract(url)
+
+    # Return just the core domain name
+    return  extracted.domain.upper()
+
+
+# Using a name other than 'input' to avoid overwriting the built-in function
+#user_input = input("Enter String: ")
+#
+# if urlmatch(user_input):
+#     print("URL is Valid")
+# else:
+#     print("URL is Invalid")
+
